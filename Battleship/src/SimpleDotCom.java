c41de06191a46c4a8bfdb718e8acaa8c8a84805e
Christian Portillo <chris.dosp@gmail.com>
2022-01-04 17:40:15 -0700

    Simple Battleship game

---
+++ Battleship/src/SimpleDotCom.java
@@ -0,0 +1,27 @@
+public class SimpleDotCom {
+
+    int [] locationCells;
+    int numOfHits = 0;
+
+    public void setLocationCells(int[] iocs) {
+        locationCells = iocs;
+    }
+
+    public String checkYourself(String stringGuess) {
+        int guess = Integer.parseInt(stringGuess);
+        String result = "Miss";
+
+        for (int cell : locationCells) {
+            if (guess == cell) {
+                result = "hit";
+                numOfHits++;
+                break;
+            }
+        }
+        if (numOfHits == locationCells.length) {
+            result = "Kill";
+        }
+        System.out.println(result);
+        return result;
+    }
+}
